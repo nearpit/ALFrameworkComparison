7fddf470f953c6b287151d9e1090eaab3188976a
Ilia Koloiarov <pudgiko@gmail.com>
2023-06-23 11:48:50 +0200

    small comment

--- datasets/splice.py
+++ datasets/splice.py
@@ -2,7 +2,7 @@ from datasets.base import SVMDataset
 from sklearn.preprocessing import FunctionTransformer, MinMaxScaler
 
 class Splice(SVMDataset):
-
+    # split train for validation
     urls_dict = {"train":"https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/splice_scale",
                  "val_test": "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/splice.t"}
     dataset_name = "splice"
