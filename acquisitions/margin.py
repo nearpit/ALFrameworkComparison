55894c9cf6115566ebba38a6050e23dd3b67fa3b
Nearpit <43110453+Nearpit@users.noreply.github.com>
2023-07-08 17:45:58 +0200

    Update margin.py

--- acquisitions/margin.py
+++ acquisitions/margin.py
@@ -7,8 +7,7 @@ class Margin(Acquisition):
     
     def get_scores(self):
         x, y = self.pool.get("unlabeled")
-        with torch.no_grad():
-            probs = self.clf(torch.Tensor(x))
+        probs = self.clf(torch.Tensor(x))
         sorted_probs, idx = probs.sort(descending=True)
         U = sorted_probs[:, 0] - sorted_probs[:, 1]
         return -U # in order to aligh to the argmax query
