979d903cd42b3d132d9d76153d004be2328fec58
Nearpit <43110453+Nearpit@users.noreply.github.com>
2023-08-14 21:17:00 +0200

    Update entropy.py

--- acquisitions/entropy.py
+++ acquisitions/entropy.py
@@ -1,5 +1,6 @@
 from acquisitions import Acquisition
 import torch
+import numpy as np
 
 class Entropy(Acquisition):
     def __init__(self, *args, **kwargs):
@@ -9,6 +10,6 @@ class Entropy(Acquisition):
         if values is None:
             values, y = self.pool.get("unlabeled")
         probs = self.clf(torch.Tensor(values)).cpu()
-        log_probs = torch.log(probs)
+        log_probs = torch.log(probs + np.finfo(np.float32).smallest_normal)
         U = -(probs*log_probs).sum(axis=1)
         return U
